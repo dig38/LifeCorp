b7fbfc4b6f085e88fd08913b1ab2bfab751b18e9
Dottermusch <rick.dottermusch@gmail.com>
2015-07-15 15:27:31 -0400

    Removes depricated Order insert method

--- LifeCorpFinal/src/data/DemoOrderDB.java
+++ LifeCorpFinal/src/data/DemoOrderDB.java
@@ -14,75 +14,52 @@ import myTools.DBUtil;
 
 public class DemoOrderDB 
 {
-	public static boolean insertOrder(DemoOrder order)
-	{
-		BigDecimal total = new BigDecimal(0);
-		BigDecimal quantity = new BigDecimal(0);
-		BigDecimal price = new BigDecimal(0);
-		BigDecimal linePrice = new BigDecimal(0);
-		BigDecimal testPrice = new BigDecimal(0);
-		
-		Date date = new Date();
-		
-		for (DemoOrderItem oi : order.getDemoOrderItems())
-		{
-			quantity = oi.getQuantity();
-			price = oi.getDemoProductInfo().getListPrice();
-			linePrice = price.multiply(quantity);
-			oi.setUnitPrice(price);
-			testPrice = oi.getUnitPrice();
-			//oi.setUnitPrice(oi.getQuantity().multiply(oi.getProduct().getListPrice()));
-			total = total.add(oi.getQuantity().multiply(oi.getDemoProductInfo().getListPrice()));
-		}
-		
-		order.setOrderTotal(total);
-		order.setOrderTimestamp(date);
-		
-		EntityManager em = DBUtil.getEmFactory().createEntityManager();
-		EntityTransaction trans = em.getTransaction();
-		
-		try
-		{
-			trans.begin();
-			em.persist(order);
-			trans.commit();
-			return true;
-		}
-		catch (Exception e)
-		{
-			trans.rollback();
-			System.out.println("A problem occurred while inserting the order: " + e);
-			return false;
-		}
-		finally
-		{
-			em.close();
-		}
-		
-	}
-	
-	public static BigDecimal presetOrderTotal(DemoOrder order)
-	{
-		BigDecimal total = new BigDecimal(0);
-		BigDecimal quantity = new BigDecimal(0);
-		BigDecimal price = new BigDecimal(0);
-		BigDecimal linePrice = new BigDecimal(0);
-		BigDecimal testPrice = new BigDecimal(0);
-		
-		for (DemoOrderItem oi : order.getDemoOrderItems())
-		{
-			quantity = oi.getQuantity();
-			price = oi.getDemoProductInfo().getListPrice();
-			linePrice = price.multiply(quantity);
-			oi.setUnitPrice(price);
-			testPrice = oi.getUnitPrice();
-			//oi.setUnitPrice(oi.getQuantity().multiply(oi.getProduct().getListPrice()));
-			total = total.add(oi.getQuantity().multiply(oi.getDemoProductInfo().getListPrice()));
-		}
-		
-		order.setOrderTotal(total);
-		return total;
-	}
+//	public static boolean insertOrder(DemoOrder order)
+//	{
+//		BigDecimal total = new BigDecimal(0);
+//		BigDecimal quantity = new BigDecimal(0);
+//		BigDecimal price = new BigDecimal(0);
+//		BigDecimal linePrice = new BigDecimal(0);
+//		BigDecimal testPrice = new BigDecimal(0);
+//		
+//		Date date = new Date();
+//		
+//		for (DemoOrderItem oi : order.getDemoOrderItems())
+//		{
+//			quantity = oi.getQuantity();
+//			price = oi.getDemoProductInfo().getListPrice();
+//			linePrice = price.multiply(quantity);
+//			oi.setUnitPrice(price);
+//			testPrice = oi.getUnitPrice();
+//			//oi.setUnitPrice(oi.getQuantity().multiply(oi.getProduct().getListPrice()));
+//			total = total.add(oi.getQuantity().multiply(oi.getDemoProductInfo().getListPrice()));
+//		}
+//		
+//		order.setOrderTotal(total);
+//		order.setOrderTimestamp(date);
+//		
+//		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+//		EntityTransaction trans = em.getTransaction();
+//		
+//		try
+//		{
+//			trans.begin();
+//			em.persist(order);
+//			trans.commit();
+//			return true;
+//		}
+//		catch (Exception e)
+//		{
+//			trans.rollback();
+//			System.out.println("A problem occurred while inserting the order: " + e);
+//			return false;
+//		}
+//		finally
+//		{
+//			em.close();
+//		}
+//		
+//	}
 	
 	public static long insertOrderReturnId(DemoOrder order)
 	{
@@ -132,6 +109,31 @@ public class DemoOrderDB
 			em.close();
 		}	
 	}
+	
+	
+	public static BigDecimal presetOrderTotal(DemoOrder order)
+	{
+		BigDecimal total = new BigDecimal(0);
+		BigDecimal quantity = new BigDecimal(0);
+		BigDecimal price = new BigDecimal(0);
+		BigDecimal linePrice = new BigDecimal(0);
+		BigDecimal testPrice = new BigDecimal(0);
+		
+		for (DemoOrderItem oi : order.getDemoOrderItems())
+		{
+			quantity = oi.getQuantity();
+			price = oi.getDemoProductInfo().getListPrice();
+			linePrice = price.multiply(quantity);
+			oi.setUnitPrice(price);
+			testPrice = oi.getUnitPrice();
+			//oi.setUnitPrice(oi.getQuantity().multiply(oi.getProduct().getListPrice()));
+			total = total.add(oi.getQuantity().multiply(oi.getDemoProductInfo().getListPrice()));
+		}
+		
+		order.setOrderTotal(total);
+		return total;
+	}
+	
 	
 	public static DemoOrder getOrderById(long orderId)
 	{
